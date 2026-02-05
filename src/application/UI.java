d23b1b11b4ffeea1e30e3a163428901ca59a2849
mr-robot <joao_flap@hotmail.com>
2026-02-05 15:43:39 -0300

    Release: Cyber Chess System v1.0 (Netrunner Edition)

---
+++ src/application/UI.java
@@ -0,0 +1,145 @@
+package application;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import chess.Color;
+
+public class UI {
+
+	// Cores do Texto (Foreground)
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	// Cores do Fundo (Background)
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		}
+		catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+		}
+	}
+
+	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
+		printHeader();
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		printCapturedPieces(captured);
+		System.out.println();
+		System.out.println("Turn : " + chessMatch.getTurn());
+
+		// Lógica dos nomes atualizada para Netrunners vs Corporação
+		String currentPlayerName;
+		if (chessMatch.getCurrentPlayer() == Color.WHITE) {
+			currentPlayerName = ANSI_CYAN + "NETRUNNERS" + ANSI_RESET;
+		} else {
+			currentPlayerName = ANSI_PURPLE + "CORPORAÇÃO" + ANSI_RESET;
+		}
+
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + currentPlayerName);
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + currentPlayerName);
+		}
+	}
+
+	private static void printHeader() {
+		System.out.println(ANSI_PURPLE + "======================================" + ANSI_RESET);
+		System.out.println(ANSI_CYAN + "      CYBER CHESS SYSTEM v1.0" + ANSI_RESET);
+		System.out.println(ANSI_PURPLE + "      Dev: Jota" + ANSI_RESET);
+		System.out.println(ANSI_PURPLE + "======================================" + ANSI_RESET);
+		System.out.println();
+	}
+
+	public static void printBoard(ChessPiece[][] pieces) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], false);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if (background) {
+			System.out.print(ANSI_GREEN_BACKGROUND);
+		}
+		if (piece == null) {
+			System.out.print("-" + ANSI_RESET);
+		}
+		else {
+			if (piece.getColor() == Color.WHITE) {
+				System.out.print(ANSI_CYAN + piece + ANSI_RESET);
+			}
+			else {
+				System.out.print(ANSI_PURPLE + piece + ANSI_RESET);
+			}
+		}
+		System.out.print(" ");
+	}
+
+	private static void printCapturedPieces(List<ChessPiece> captured) {
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		System.out.println("Captured pieces:");
+
+		System.out.print("Netrunners (Cyan): ");
+		System.out.print(ANSI_CYAN);
+		System.out.println(Arrays.toString(white.toArray()));
+		System.out.print(ANSI_RESET);
+
+		System.out.print("Corporação (Purple): ");
+		System.out.print(ANSI_PURPLE);
+		System.out.println(Arrays.toString(black.toArray()));
+		System.out.print(ANSI_RESET);
+	}
+}
